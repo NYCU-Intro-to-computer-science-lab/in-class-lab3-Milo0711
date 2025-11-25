ee113b732a394e293cd91569381a663a5980bb4c
呂子誠 <milo3s4fg@gmail.com>
2025-11-25 14:01:24 +0800

    完成p2

--- src/p2.cpp
+++ src/p2.cpp
@@ -3,8 +3,21 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int gcd(int x, int y){
     
+    if (y == 0){
+        return x;
+    }
+
+    return gcd (y , x % y);
+
+}
     
+
+int main(){
+    int x, y;
+    cin >> x >> y;
+    cout << gcd(x, y);
+
     return 0;
 }
