0534516d6bdac2e7b7796d59f54cdfffdbfe4c8a
呂子誠 <milo3s4fg@gmail.com>
2025-11-25 14:01:01 +0800

    完成p1

--- src/p1.cpp
+++ src/p1.cpp
@@ -3,8 +3,30 @@
 #include <iostream>
 using namespace std;
 
+
+long long factorial(int n){
+    if (n < 1){
+        return 1;
+    }
+
+    return n * factorial(n-1);
+}
+
+long long sum(int n){
+
+    if (n < 1){
+        return 0;
+    }
+
+    return n + sum (n - 1);
+}
+
+
 int main() {
     
-    
+    int n;
+    cin >> n;
+    cout << factorial(n) << endl;
+    cout << sum(n) << endl;
     return 0;
 }
